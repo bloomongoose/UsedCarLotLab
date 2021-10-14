db34b72e940418d48e85d7dc9beeeac4b752e627
radeen <radeen013@gmail.com>
2021-10-14 15:07:15 -0400

    modified UsedCar class

---
+++ UsedCarLotLab/UsedCar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsedCarLotLab
+{
+    class UsedCar : Car
+    {
+        public UsedCar(string cMake, string cModel, int cYear, double cPrice, double cMileage) : base(cModel, cMake, cYear, cPrice)
+        {
+            Mileage = cMileage;
+        }
+        public double Mileage;
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} | Mileage: {Mileage}";
+        }
+    }
+
+}

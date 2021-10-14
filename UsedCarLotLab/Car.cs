fd3c0d0da74b827cb69450ad5b3b3a47e70804ff
Yashashree Majalikar <yashmaja@gmail.com>
2021-10-14 14:55:16 -0400

    separated Car Class

---
+++ UsedCarLotLab/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsedCarLotLab
+{
+    class Car
+    {
+        public string Make;
+        public string Model;
+        public int Year;
+        public double Price;
+
+        public Car(string cMake, string cModel, int cYear, double cPrice)
+        {
+            Make = cMake;
+            Model = cModel;
+            Year = cYear;
+            Price = cPrice;
+        }
+        public override string ToString()
+        {
+            return $"Make: {Make} | Model: {Model} | Year: {Year} | Price: {Price}";
+        }
+    }
+}
